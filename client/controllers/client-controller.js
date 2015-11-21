091d2a5502dcbdcbe4087cd9bdbc7f07b679b6fc
ZiJuGa <zjulbertg@gmail.com>
2015-11-22 04:43:31 +0800

    wala pa heatmaps

--- client/controllers/client-controller.js
+++ client/controllers/client-controller.js
@@ -14,16 +14,33 @@ app.controller('index-controller', function ($scope, $http) {
 
 		var all = response;
 
-
-
 		var gmarkers = [];
 		for (var i = 0; i < all.length; i++) {
 	        var lat = all[i].latitude;
 	        var lng = all[i].longitude;
 	        var latLng = new google.maps.LatLng(lat, lng);
-	        var marker = new google.maps.Marker({map: map, position: latLng,});
+	        var marker = new google.maps.Marker({map: map, position: latLng, dataId: i});
 	        gmarkers.push(marker);
-		}
+		};
+		var contents = [[]];
+
+		for (var i = 0; i < response.length; i++) {
+			contents[i] = [response[i].name, response[i].time, response[i].crime];
+		};
+
+		var infowindow = new google.maps.InfoWindow();
+
+		for (var i = 0; i < gmarkers.length; i++) {
+			google.maps.event.addListener(gmarkers[i], 'mouseover', function () {
+				infowindow.setContent(contents[this.dataId][1] + '<br/>' + contents[this.dataId][2]);
+	            infowindow.open(map, this);
+	        });
+
+	        google.maps.event.addListener(gmarkers[i], 'mouseout', function() {
+	          	// your code goes here!
+	          	infowindow.close();
+	        });
+		};
 
 		var markerCluster = new MarkerClusterer(map, gmarkers);
 
@@ -65,6 +82,7 @@ app.controller('form-controller', function ($scope, $http) {
 		$scope.report.latitude = document.getElementById("latbox").value;
 		$scope.report.longitude = document.getElementById("lngbox").value;
 		$http.post('/addcoordinates', $scope.report);
+		window.location = "/";
 	};
 });
 
--- server/controllers/server-controller.js
+++ server/controllers/server-controller.js
@@ -1,4 +1,4 @@
-var coordinates = [{longitude: 121.0000, latitude: 14.5800}, {longitude: 120.0000, latitude: 13}, {longitude: 122.0000, latitude: 15.5000}];
+var coordinates = [{name: "kass", time: 2013, crime: "Holdapping", longitude: 121.0000, latitude: 14.5800}, {name: "ghia", time: 2014, crime: "Kidnapping",  longitude: 120.0000, latitude: 13}, {name: "zj", time: 2015, crime: "Snatching",  longitude: 122.0000, latitude: 15.5000}];
 
 module.exports.getcoordinates = function (req, res) {
 	res.json(coordinates);
