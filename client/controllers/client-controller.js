d8bd762737a67bb58d7de91e8943e8ce2b39ef7a
ZiJuGa <zjulbertg@gmail.com>
2015-11-21 23:58:12 +0800

    clusterer add

--- client/controllers/client-controller.js
+++ client/controllers/client-controller.js
@@ -1,9 +1,63 @@
-app.controller('controller', function ($scope, $http) {
+
+app.controller('index-controller', function ($scope, $http) {
 	$http.get('/getcoordinates').success(function (response) {
 		$scope.reportlist = response;
+		var latlng = new google.maps.LatLng(57.8, 14.0);
+
+		var options = {
+		  	zoom: 6,
+		  	center: latlng,
+		  	mapTypeId: google.maps.MapTypeId.ROADMAP
+		}; 
+
+		var map = new google.maps.Map(document.getElementById('map'), options);
+
+		var all = response;
+
+
+		var gmarkers = [];
+		for (var i = 0; i < all.length; i++) {
+	        var lat = all[i].latitude;
+	        var lng = all[i].longitude;
+	        var latLng = new google.maps.LatLng(lat, lng);
+	        var marker = new google.maps.Marker({map: map, position: latLng,});
+	        gmarkers.push(marker);
+		}
+
+		var markerCluster = new MarkerClusterer(map, gmarkers);
+
 	});
 
-	$scope.AddCoordinates = function () {
+	
+});
+
+/*CONTROLLER OF FORM.HTML*/
+app.controller('form-controller', function ($scope, $http) {
+	var latlng = new google.maps.LatLng(14.5800, 121.0000);
+
+	var options = {
+	  	zoom: 15,
+	  	center: latlng,
+	  	mapTypeId: google.maps.MapTypeId.ROADMAP
+	}; 
+
+	var map = new google.maps.Map(document.getElementById('map'), options);
+
+	var marker = new google.maps.Marker({
+	  	draggable: true,
+	 	position: latlng,
+	 	map: map,
+		title: "Your location"
+	});
+
+	google.maps.event.addListener(marker, 'dragend', function (event) {
+        document.getElementById("latbox").value = this.getPosition().lat();
+        document.getElementById("lngbox").value = this.getPosition().lng();
+    });
+
+	$scope.AddReport = function () {
+
 		$http.post('/addcoordinates', $scope.report);
 	};
 });
+
--- server/controllers/server-controller.js
+++ server/controllers/server-controller.js
@@ -1,4 +1,4 @@
-var coordinates = [];
+var coordinates = [{longitude: 50, latitude: 10}, {longitude: 52, latitude: 10}, {longitude: 50, latitude: 12}];
 
 module.exports.getcoordinates = function (req, res) {
 	res.json(coordinates);
