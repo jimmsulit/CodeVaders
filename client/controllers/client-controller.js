4c69ddd0c17dd50469422833e17a43466fbaefc6
ZiJuGa <zjulbertg@gmail.com>
2015-11-21 14:57:53 +0800

    added the files

---
+++ client/controllers/client-controller.js
@@ -0,0 +1,3 @@
+app.controller('controller', function ($scope, $http) {
+
+});
---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var server = express();
+var servercontroller = require('./server/controllers/server-controller.js');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+/*MIDDLEWARES*/
+server.use(express.static(__dirname + '/client/views'));
+server.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
+server.use('/angular', express.static(__dirname + '/node_modules/angular/angular.min.js'));
+server.use('/client-controllers', express.static(__dirname + '/client/controllers'));
+
+/*UTILIZE BODYPARSER*/
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+/*CONNECT TO DATABASE*/
+/*var dbname = databasename*/
+mongoose.connect('mongodb://localhost:27017/' /*+ dbname*/);
+mongoose.connection.on('connected', function() {
+	console.log('connected to port 27017');
+});
+
+
+
+
+server.listen(3000, function () {
+	console.log('listening on  port 3000');
+});
